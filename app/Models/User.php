a457fa53b6ac027f12d30a34ff8309410436c577
Alex Candela <alex.candelaa@gmail.com>
2024-08-15 21:14:12 +0200

    cambio en url de .env

--- app/Models/User.php
+++ app/Models/User.php
@@ -97,11 +97,11 @@ class User extends Authenticatable implements JWTSubject
         ]);
         if (isset($data['img'])) {
             $user->update([
-                'img' => 'https://geturplanbackend.online/storage/images/users/' . $user->id . '/' . $data['img']
+                'img' => env('APP_URL') . '/storage/images/users/' . $user->id . '/' . $data['img']
             ]);
         } else if (isset($data['default_img'])) {
             $user->update([
-                'img' => 'https://geturplanbackend.online/storage/default/default_user.png'
+                'img' => env('APP_URL') . '/storage/default/default_user.png'
             ]);
         }
         
